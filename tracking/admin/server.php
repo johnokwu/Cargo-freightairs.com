fe0676d80963b2d8525783887cf63cb35ca78ff3
Gibson John <okwujohn111@gmail.comm>
2024-05-13 10:05:24 +0100

    no commit again

--- tracking/admin/server.php
+++ tracking/admin/server.php
@@ -10,14 +10,16 @@ $conn = mysqli_connect('127.0.0.1:3306', 'u257513287_express_user', 'bC&63z8n+^3
 }
 echo "Connected successfully"; */
 
-function saiful($tring) {
-    $string = stripslashes($string);
-    $string = strip_tags($string);
-    $string = htmlspecialchars($string);
+function saiful($tring)
+{
+  $string = stripslashes($string);
+  $string = strip_tags($string);
+  $string = htmlspecialchars($string);
   //  $string = mysqli_real_escape_string($conn, $string);
-    return $string;
+  return $string;
 }
+
+
 
 #$chat_id = '-1001494654627';
 #$bot_token = '1418953624:AAGKgxKhMTIld9gq0gxHjm7pDqDU76If_Z0';
-?>
