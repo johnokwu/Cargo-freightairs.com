301c3da76888700b0e383778f526e5eb2b07762d
Gibson John <okwujohn111@gmail.comm>
2024-05-13 09:20:06 +0100

    third commit

--- services.php
+++ services.php
@@ -1,5 +1,5 @@
 <?php
-  include('include/header.php');
+include('include/header.php');
 ?>
 <!-- end page-header -->
 <section class="content-section">
@@ -9,41 +9,41 @@
             <div class="section-title">
                <figure><img src="images/section-title-shape.png" alt="Image"></figure>
                <h2>Reliable UK & Ireland Transport<br>
-                  Logistics Since 1973 
+                  Logistics Since 1973
                </h2>
             </div>
-            <!-- end section-title --> 
+            <!-- end section-title -->
          </div>
          <!-- end col-12 -->
          <div class="col-lg-6">
-            <p>At Cardinal, we believe the supplier-vendor relationship to be an 
-               important, strong and long term partnership. Both the client and 
-               the logistics partners will concentrate on their lead competencies 
-               and work closely together with the common aim to be a leader in 
-               each other’s sector. We want to service you in a reliable, efficient 
-               and flexible manner, so that your company can compete in its 
-               business, not only by selling quality products, but also by satisfying 
+            <p>At Cargo Freight Airs, we believe the supplier-vendor relationship to be an
+               important, strong and long term partnership. Both the client and
+               the logistics partners will concentrate on their lead competencies
+               and work closely together with the common aim to be a leader in
+               each other’s sector. We want to service you in a reliable, efficient
+               and flexible manner, so that your company can compete in its
+               business, not only by selling quality products, but also by satisfying
                customers with high value logistics services.
             </p>
-            <p> With so many options available you can rest assured that we 
-               will be able to deliver your consignment, regardless of its size. 
-               And if there’s ever a time where you need some advice on 
-               choosing the right solution, our transport team, who have more 
-               than 120 years’ experience. 
+            <p> With so many options available you can rest assured that we
+               will be able to deliver your consignment, regardless of its size.
+               And if there’s ever a time where you need some advice on
+               choosing the right solution, our transport team, who have more
+               than 120 years’ experience.
             </p>
-            <br> 
+            <br>
          </div>
          <!-- end col-6 -->
          <div class="col-lg-6">
-            <p>Since our inception in 1998, we have developed according to our 
-               clients’ needs from a traditional freight forwarding company to a 
-               full service logistics provider. We provide all the necessary services 
-               for highly-engineered supply chain solutions and combine these 
-               with a range of warehousing and freight forwarding services to 
+            <p>Since our inception in 1998, we have developed according to our
+               clients’ needs from a traditional freight forwarding company to a
+               full service logistics provider. We provide all the necessary services
+               for highly-engineered supply chain solutions and combine these
+               with a range of warehousing and freight forwarding services to
                provide a complete solution.
             </p>
          </div>
-         <!-- end col-6 --> 
+         <!-- end col-6 -->
          <div class="col-lg-3 col-md-6">
             <div class="service-box">
                <div class="inner">
@@ -105,9 +105,9 @@
          </div>
          <!-- end col-3 -->
       </div>
-      <!-- end row --> 
+      <!-- end row -->
    </div>
-   <!-- end container --> 
+   <!-- end container -->
 </section>
 <!-- end content-section -->
 <section class="content-section no-spacing" data-background="#f9f7ef">
@@ -115,31 +115,31 @@
       <div class="row align-items-center no-gutters">
          <div class="col-lg-6">
             <div class="side-content">
-               <h2>Supply Chain 
+               <h2>Supply Chain
                   Solutions
                </h2>
-               <p>At Cardinal, we’re passionate about helping our 
-                  clients operate their supply chains more effectively. 
-                  To achieve this we design and implement 
-                  transformative solutions that are customised for 
-                  every individual client and their unique supply chain 
-                  challenges. Using an innovative approach to manage 
-                  supply chains we’ve given our clients true supply 
-                  chain visibility with revolutionary views of their supply 
+               <p>At Cargo Freight Airs, we’re passionate about helping our
+                  clients operate their supply chains more effectively.
+                  To achieve this we design and implement
+                  transformative solutions that are customised for
+                  every individual client and their unique supply chain
+                  challenges. Using an innovative approach to manage
+                  supply chains we’ve given our clients true supply
+                  chain visibility with revolutionary views of their supply
                   chain. We continually work towards achieving.
                </p>
             </div>
-            <!-- end side-content --> 
+            <!-- end side-content -->
          </div>
          <!-- end col-6 -->
          <div class="col-lg-6">
             <figure class="side-image full-right"> <img src="images/side-image02.jpg" alt="Image"> </figure>
          </div>
-         <!-- end col-6 --> 
+         <!-- end col-6 -->
       </div>
-      <!-- end row --> 
+      <!-- end row -->
    </div>
-   <!-- end container --> 
+   <!-- end container -->
 </section>
 <!-- end content-section -->
 <section class="content-section no-spacing" data-background="#ffffff">
@@ -148,14 +148,14 @@
          <div class="col-lg-6">
             <figure class="side-image full-left"> <img src="images/side-image02.jpg" alt="Image"> </figure>
          </div>
-         <!-- end col-6 --> 
+         <!-- end col-6 -->
          <div class="col-lg-6">
             <div class="side-content right">
                <h2>Internet Fulfilment</h2>
-               <p>We pick, pack and dispatch your orders, to save you 
-                  time and money. We take away the hassle of storing, 
-                  delivering and handling any product returns and 
-                  manage your fulfilment so you can concentrate on 
+               <p>We pick, pack and dispatch your orders, to save you
+                  time and money. We take away the hassle of storing,
+                  delivering and handling any product returns and
+                  manage your fulfilment so you can concentrate on
                   your key business objectives.
                </p>
                <ul class="list">
@@ -165,13 +165,13 @@
                   <li>Full order management & back office integration</li>
                </ul>
             </div>
-            <!-- end side-content --> 
+            <!-- end side-content -->
          </div>
          <!-- end col-6 -->
       </div>
-      <!-- end row --> 
+      <!-- end row -->
    </div>
-   <!-- end container --> 
+   <!-- end container -->
 </section>
 <!-- end content-section -->
 <section class="content-section no-spacing" data-background="#212223">
@@ -180,28 +180,28 @@
          <div class="col-lg-6">
             <div class="side-content light">
                <h2>Global Freight Forwarding</h2>
-               <p>At Cardinal, we’re passionate about helping our 
-                  clients operate their supply chains more effectively. 
-                  To achieve this we design and implement 
-                  transformative solutions that are customised for 
-                  every individual client and their unique supply chain 
-                  challenges. Using an innovative approach to manage 
-                  supply chains we’ve given our clients true supply 
-                  chain visibility with revolutionary views of their supply 
+               <p>At Cargo Freight Airs, we’re passionate about helping our
+                  clients operate their supply chains more effectively.
+                  To achieve this we design and implement
+                  transformative solutions that are customised for
+                  every individual client and their unique supply chain
+                  challenges. Using an innovative approach to manage
+                  supply chains we’ve given our clients true supply
+                  chain visibility with revolutionary views of their supply
                   chain. We continually work towards achieving.
                </p>
             </div>
-            <!-- end side-content --> 
+            <!-- end side-content -->
          </div>
          <!-- end col-6 -->
          <div class="col-lg-6">
             <figure class="side-image full-right"> <img src="images/side-image02.jpg" alt="Image"> </figure>
          </div>
-         <!-- end col-6 --> 
+         <!-- end col-6 -->
       </div>
-      <!-- end row --> 
+      <!-- end row -->
    </div>
-   <!-- end container --> 
+   <!-- end container -->
 </section>
 <!-- end content-section -->
 <section class="content-section">
@@ -213,7 +213,7 @@
                <h2>Our Global World</h2>
                <p>Advice and best practice from our own team</p>
             </div>
-            <!-- end section-title --> 
+            <!-- end section-title -->
          </div>
          <!-- end col-12 -->
       </div>
@@ -223,45 +223,45 @@
             <div class="image-content-box text-center">
                <figure> <img src="images/service-image01.jpg" alt="Image"> </figure>
                <h6>Forwarding</h6>
-               <p>Our dedicated fleet of vehicles 
-                  operates nationally throughout the 
-                  UK delivering both full, part, and 
+               <p>Our dedicated fleet of vehicles
+                  operates nationally throughout the
+                  UK delivering both full, part, and
                   consolidated loads.
                </p>
             </div>
-            <!-- end image-content-box --> 
+            <!-- end image-content-box -->
          </div>
          <!-- end col-4 -->
          <div class="col-lg-4 col-md-6">
             <div class="image-content-box text-center">
                <figure> <img src="images/service-image02.jpg" alt="Image"> </figure>
                <h6>Supply Chain</h6>
-               <p>Sending smaller consignments of 
-                  less than 10 pallets used to be 
-                  expensive business, but we have a 
+               <p>Sending smaller consignments of
+                  less than 10 pallets used to be
+                  expensive business, but we have a
                   solution for you
                </p>
             </div>
-            <!-- end image-content-box --> 
+            <!-- end image-content-box -->
          </div>
          <!-- end col-4 -->
          <div class="col-lg-4 col-md-6">
             <div class="image-content-box text-center">
                <figure> <img src="images/service-image03.jpg" alt="Image"> </figure>
                <h6>Forwarding</h6>
-               <p>In addition to our UK services, through 
-                  our trusted and fully-vetted network of 
-                  partners, we offer a full import and 
-                  export service 
+               <p>In addition to our UK services, through
+                  our trusted and fully-vetted network of
+                  partners, we offer a full import and
+                  export service
                </p>
             </div>
-            <!-- end image-content-box --> 
+            <!-- end image-content-box -->
          </div>
-         <!-- end col-4 --> 
+         <!-- end col-4 -->
       </div>
-      <!-- end row --> 
+      <!-- end row -->
    </div>
-   <!-- end container --> 
+   <!-- end container -->
 </section>
 <!-- end content-section -->
 <section class="content-section no-top-spacing">
@@ -271,19 +271,19 @@
             <div class="contact-bar">
                <h2>Contact us today!</h2>
                <p>Contact us today for your airfreight <br>
-                  requirements 
+                  requirements
                </p>
-               <a href="contact.php">CLICK HERE TO CONTACT US!</a> 
+               <a href="contact.php">CLICK HERE TO CONTACT US!</a>
             </div>
-            <!-- end contact-bar --> 
+            <!-- end contact-bar -->
          </div>
-         <!-- end col-12 --> 
+         <!-- end col-12 -->
       </div>
-      <!-- end row --> 
+      <!-- end row -->
    </div>
-   <!-- end container --> 
+   <!-- end container -->
 </section>
 <!-- end content-section -->
 <?php
-  include('include/footer.php');
+include('include/footer.php');
 ?>
